548482e9acf5260ada1ebad9cd5bb40a88cabbfb
root <icarus.so@isd.tvb.com>
2018-07-25 12:53:08 +0800

    adjust memory

--- bigQueryExporter/BigQueryExporter.py
+++ bigQueryExporter/BigQueryExporter.py
@@ -3,6 +3,7 @@ import time
 import uuid
 import logging
 import shutil
+import pandas as pd
 from datetime import datetime 
 from google.cloud import bigquery
 from google.cloud import storage
@@ -17,18 +18,22 @@ class BigQueryExporter:
     def use_cache(is_use_cache=True):
         BigQueryExporter._use_cache = is_use_cache
     
-    def __init__(self, project_name, dataset_name, bucket_name):
+    def __init__(self, project_name, dataset_name, bucket_name, log_lambda=None):
         self.project_name = project_name
         self.dataset_name = dataset_name
         self.bucket_name = bucket_name
+        self.log_lambda = log_lambda
         
         self.bigquery_client = bigquery.Client(project=project_name)
         self.storage_client = storage.Client(project=project_name)
         
         
-    def query_to_table(self, query, job_name):
-        # Do nothing if use_cache
+    def query_to_table(self, query, job_name, dataset_name=None):
+        # external logging if required
+        if self.log_lambda is not None:
+            self.log_lambda(query)
         
+        # Do nothing if use_cache
         if BigQueryExporter._use_cache:
             return
         
@@ -37,7 +42,9 @@ class BigQueryExporter:
         startTime= datetime.now()
         
         # initialize variables
-        dataset_name = self.dataset_name
+        if dataset_name is None:
+            dataset_name = self.dataset_name
+        logging.info('[BigQueryExporter] ['+job_name+'] ::dataset is set to %s' % dataset_name )
         bigquery_client = self.bigquery_client
         
         # Point to the dataset and table
@@ -46,10 +53,14 @@ class BigQueryExporter:
 
         # Create an empty table
         try:
+            logging.info('[BigQueryExporter] ['+job_name+'] ::bigqueyr_client.get_table(%s) ...' % destination_table )
             self.bigquery_client.get_table(destination_table)
+            logging.info('[BigQueryExporter] ['+job_name+'] ::bigqueyr_client.delete_table(%s) ...' % destination_table )
             self.bigquery_client.delete_table(destination_table)
         except:
+            logging.info('[BigQueryExporter] ['+job_name+'] ::execption point 01 ...' % dataset_name )
             pass
+        logging.info('[BigQueryExporter] ['+job_name+'] ::bigqueyr_client.create_table( Table( %s ) ) ...' % destination_table )
         self.bigquery_client.create_table(Table(destination_table))
         # destination_table.create()
         
@@ -61,6 +72,8 @@ class BigQueryExporter:
         job_config.use_legacy_sql = False
         job_config.destination = destination_table
         
+        logging.info('[BigQueryExporter] ['+job_name+'] ::bigqueyr_client.query() starts ...' )
+        logging.info('[BigQueryExporter] ['+job_name+'] ::job_config: %s' % str(job_config) )
         job = self.bigquery_client.query(query, job_config=job_config)
         
         # Wait till the job done
@@ -166,4 +179,28 @@ class BigQueryExporter:
         # logging
         timeElapsed=datetime.now()-startTime 
         logging.info('[BigQueryExporter] ['+job_name+'] ::query_to_local completed, elpased {}s'.format(timeElapsed.seconds))
+        
+        
+    def query_to_memory(self, query):
+        # external logging if required
+        if self.log_lambda is not None:
+            self.log_lambda(query)
+        
+        # job_config = bigquery.QueryJobConfig()
+        # job_config.use_legacy_sql = False
+        
+        iterator = self.bigquery_client.query(query)
+        rows = list(iterator)
+        
+        return rows
+    
+    def query_to_df(self, query):
+        _ = self.query_to_memory(query)
+        __ = _[0]
+        columns = [k for (k, v) in __.items()]
+        values = [[v for (k, v) in __.items()] for __ in _]
+        df = pd.DataFrame(values, columns=columns)
+        return df
+    
+    
         
